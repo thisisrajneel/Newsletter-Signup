75d69b4dc51c51eb26ad8a2f43dfae2c963174a7
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-26 16:21:03 +0530

    added frontend to server

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const request = require('request')
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/signup.html')
+})
 
 app.listen(3000, ()=>{
     console.log('server running on port 3000');
